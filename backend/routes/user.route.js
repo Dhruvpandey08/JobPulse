50b0ff6ac8415cc9160d5148f3cc11de61242a41
Dhruv Pandey <dhruvpandey928@gmail.com>
2024-07-27 12:45:21 +0530

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -6,7 +6,7 @@ import { singleUpload } from "../middlewares/multer.js";
 const router = express.Router();
 
 router.route("/register").post(singleUpload,register);
-router.route("https://jobs4you-frontend.onrender.com/login").post(login);
+router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
 
