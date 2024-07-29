59787b632e9951b54a0c37e78c438807a4c9f6ae
Dhruv <Dhruvpandey928@gmail.com>
2024-07-29 19:53:23 +0530

    modified index

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'https://jobpulse-frontend.onrender.com',
-    credentials:true
-}
+// const corsOptions = {
+//     origin:'https://jobpulse-frontend.onrender.com',
+//     credentials:true
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+// Enable CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 const PORT = process.env.PORT || 3000;
 
