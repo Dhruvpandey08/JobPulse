8eb5840af5a81caa6b0c2ded8244774f6d86be96
Dhruv <Dhruvpandey928@gmail.com>
2024-07-30 00:21:32 +0530

    changed deployment url

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ['https://jobpulse-frontend.onrender.com', 'http://localhost:5173'],
+    origin: ['https://jobs4you-frontend.onrender.com', 'http://localhost:5173'],
     credentials:true
 }
 
